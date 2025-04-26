8c216779d5081ef6b2b6a3633aa91e718485f192
Sahil Kumar <sahilkumar952558@gmail.com>
2025-04-26 17:19:21 +0530

    threesumclosest

---
+++ ThreeSum_closest.java
@@ -0,0 +1,51 @@
+
+import java.util.*;
+
+public class ThreeSum_closest {
+
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int closest = nums[0] + nums[1] + nums[2];
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == target) {
+                    return sum;
+                }
+
+                if (Math.abs(target - sum) < Math.abs(target - closest)) {
+                    closest = sum;
+                }
+
+                if (sum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return closest;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array:");
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        System.out.println("Enter the target value:");
+        int target = sc.nextInt();
+        ThreeSum_closest solution = new ThreeSum_closest();
+        System.out.println(solution.threeSumClosest(nums, target));
+        sc.close();
+    }
+}
